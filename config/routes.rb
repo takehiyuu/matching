77977fcc473332b8280854f54d09d3649e2ce8c6
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-22 17:48:06 +0900

    routes.rbにcompanies追記、root_pathをmains/indexに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "matches#index"
+  root to: "mains#index"
   resources :matches
   resources :mains
   resources :profiles
+  resources :companies
 end
