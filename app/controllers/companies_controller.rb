9b93728110bb2eeda2cef2501b3b7166af2ddd47
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-22 17:49:27 +0900

    companies controller作成

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,20 @@
+class CompaniesController < ApplicationController
+     def new
+          @company = Company.new
+     end
+
+     def create
+          company = Company.create(company_params)
+          if company.save
+               redirect_to root_path
+          else
+               render 'new'
+          end
+     end
+
+     private
+
+     def company_params
+          params.require(:company).permit(:name, :occupaion, :prefecture_id, :phone_number, :city, :address, :image, :building).merge(user_id: current_user.id)
+     end
+end
