7bea41a787f384fbd372f3d92bca36198b4f5715
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-22 18:54:52 +0900

    マイページに画像を保存できるように記述

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,6 +15,6 @@ class ProfilesController < ApplicationController
      private
 
      def profile_params
-          params.require(:profile).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :gender_id, :prefecture_id, :phone_number, :city, :pr, :birth_date).merge(user_id: current_user.id)
+          params.require(:profile).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :gender_id, :prefecture_id, :image, :phone_number, :city, :pr, :birth_date).merge(user_id: current_user.id)
      end
 end
