6a88d3a8d04949370c947dac1fbe728e098694ba
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-25 18:11:38 +0900

    showアクション記述

--- app/controllers/squares_controller.rb
+++ app/controllers/squares_controller.rb
@@ -2,4 +2,8 @@ class SquaresController < ApplicationController
      def index
           @recruitments = Recruitment.all
      end
+
+     def show
+          @recruitment = Recruitment.find(params[:id])
+     end
 end
