37148fc80a1abad236bd872a00483c1f7fb7ce63
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-02-25 19:31:19 +0900

    userモデルにパスワードと名前のバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   has_one :profile
   has_one :company
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8}+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には半角英数8文字で設定してください'
+
+  validates :name, presence: true, length: { maximum: 10 }
 end
