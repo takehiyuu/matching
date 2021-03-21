8853b6265efcfbb2620bc0e3c43da579125daf02
takehiyuu <77262296+takehiyuu@users.noreply.github.com>
2021-03-21 17:46:25 +0900

    recruitmentが削除された時にhuntingも削除されるように設定

--- app/models/recruitment.rb
+++ app/models/recruitment.rb
@@ -1,6 +1,6 @@
 class Recruitment < ApplicationRecord
      belongs_to :company
-     has_many :huntings
+     has_many :huntings, dependent: :destroy
      has_many_attached :images
 
      validates :prefecture_id, numericality: { other_than: 1 } 
